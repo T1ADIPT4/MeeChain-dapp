12ea61ac644079b1ce59226d1810ced8e827d7e3
Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>
2025-09-25 13:04:11 +0700

    Update schema.js
    Signed-off-by: Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>

--- packages/config/prisma/client/schema.js
+++ packages/config/prisma/client/schema.js
@@ -3,7 +3,6 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prisma = new PrismaClient().$extends(withAccelerate())
 
-// Define database connection via the `DATABASE_URL` env var
 datasource db {
   provider = "postgresql"
   url      = env("DATABASE_URL")//POSTGRES_URL="postgres://2d21971a3a2150beba1038a4ca5e8a805401198ddc9c81abbc7b090d083e3e05:sk_plNa2zUrZfK9luNji5Nf_@db.prisma.io:5432/postgres?sslmode=require"
