7e8f9d8d9c3bc42be8d9c0a3c54a4190217fd200
Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>
2025-09-25 13:43:00 +0700

    Update prisma.config.ts
    Signed-off-by: Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,10 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prisma = new PrismaClient().$extends(withAccelerate())
 
+const { WORKSPACE, NETWORK } = process.env;
+require('ts-node').register();
+require(`../prisma/seeds/${WORKSPACE}/${NETWORK}.ts`);
+
 const users = await prisma.user.findMany({
   where: {
     email: { endsWith: "prisma.io" }
